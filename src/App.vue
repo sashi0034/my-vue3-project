0963b733131a85eed7546c51323dd635084ada8f
sashi0034 <satoshi3104.nishi@gmail.com>
2022-08-23 22:05:16 +0900

    カウンターボタンの実験

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 
 <template>
   <div>
-    <header>
+    <header class="big-text header-band">
       Test Project
     </header>
-    <p>
-      Hello World!
-    </p>
+    <div class="pad10px">
+      <p>
+        Hello World!
+      </p>
+      
+      <CounterButton class="content-box" :maxCount="buttonMaxCount"/>
+
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue';
+import CounterButton from './CounterButton.vue';
+
+export default defineComponent({
+  name: "App",
+
+  components: {
+    CounterButton
+  },
+
+  setup(){
+    const buttonMaxCount = 5;
+
+    return {
+      buttonMaxCount: buttonMaxCount
+    }
+  }
+})
+
+
+</script>
+
---
+++ src/CounterButton.vue
@@ -0,0 +1,56 @@
+
+<template>
+  <div>
+    <button @click="onClick">increase count</button>
+    <div class="pad10px">
+      count: {{ count }}<br>
+      maxCount: {{ maxCount }}<br>
+      <div v-if="isGreater()" class="big-text color-yellow">
+        Congratulations!<br/>
+        <button @click="reset">reset</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+
+import {defineComponent, ref} from "vue"
+
+export default defineComponent({
+  name: "CounterButton",
+  components: {},
+  props: {
+    maxCount: Number,
+  },
+
+  setup(props){
+    const count = ref(0)
+
+    const onClick = () => {
+      count.value++;
+    }
+
+    const isGreater = () => {
+      return count.value >= (props.maxCount as number);
+    }
+
+    const reset = () => {
+      count.value = 0;
+    }
+
+    return {
+      count,
+      onClick,
+      isGreater,
+      reset,
+    }
+  }
+});
+
+
+</script>
+
+
+
